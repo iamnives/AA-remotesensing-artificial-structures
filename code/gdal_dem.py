68f30fbe0293b0f0d13c32cedabf3435954d90d7
AMNevesProg <ami.neves.prog@gmail.com>
2019-04-17 23:47:13 +0100

    starting data analysis

--- code/gdal_dem.py
+++ code/gdal_dem.py
@@ -8,7 +8,7 @@ SRC_FOLDER = DATA_FOLDER + "dem/"
 def main(argv):
     # Build VRT
     vrt_options = gdal.BuildVRTOptions(resolution='lowest')
-    src_dss = [SRC_FOLDER + f for f in os.listdir(SRC_FOLDER) if ".tif" in f ]
+    src_dss = [SRC_FOLDER + f for f in os.listdir(SRC_FOLDER) if ".hgt" in f ]
     gdal.BuildVRT(SRC_FOLDER + 'dem.vrt', src_dss, options=vrt_options)
 
 if __name__== "__main__":
